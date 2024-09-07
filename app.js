901920547c322795a65fe3040085d0d14cc9f0a9
Ranjeet Maurya <ranjeet@edcast.com>
2024-09-07 15:00:09 +0530

    Seperated the routes

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const rootRoute = require('./routes/root');
 
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.get('/', rootRoute);
 
 
 // Start the server
