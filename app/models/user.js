badae3fe834c6b789efb34b5fe97f4b92a83cade
Ranjeet Maurya <ranjeet@edcast.com>
2024-09-08 09:38:31 +0530

    using define method instead of extending class

--- app/models/user.js
+++ app/models/user.js
@@ -1,24 +1,16 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+  const User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
     }
-  }
-  User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  }, {});
+  
   return User;
 };
